a770d6724c7f9aaa946a7608f8ad0fa1d1a4c3aa
cheboi <moscheb96@gmail.com>
2022-10-05 17:08:28 +0300

    updating and deleting list

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BooksService } from 'src/app/services/books.service';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  currentBook: any;
+  message = '';
+
+  constructor(private booksService: BooksService, private route: ActivatedRoute, private router: Router ) { }
 
   ngOnInit(): void {
+    this.message='';
+    this.getBook(this.route.snapshot.paramMap.get('id'));
   }
 
+  getBook(id:string | null): void{
+    this.booksService.getItem(id).subscribe(
+      (book: null) =>{
+        this.currentBook = book;
+        console.log(book);
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    );
+  }
+
+  //delete
+  deleteBook(): void {
+    this.booksService.delete(this.currentBook.id).subscribe(
+      response => {
+        console.log(response);
+        this.router.navigate(['/books']);
+      },
+      error => {
+        console.log(error);
+      });
+    }
+
+    //update book
+    updateBook(): void { {
+      this.booksService.update(this.currentBook.id, this.currentBook)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.message = 'The product was updated!';
+        },
+        error => {
+          console.log(error);
+        });
+    }
+  }
 }
